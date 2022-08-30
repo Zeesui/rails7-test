d41169b57ffb69d9fcb03d0580945c5c4eb1656a
zee <kooo9671354@gmail.com>
2022-08-30 20:36:24 +0800

    cchange

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :destroy, :update]
+  before_action :find_group_and_check, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -10,7 +11,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+    #find_group_and_check
   end
 
   def new
@@ -19,6 +20,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
     else
@@ -28,7 +30,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
+    #find_group_and_check
     if @group.update(group_params)
       redirect_to groups_path
     else
@@ -38,12 +40,19 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
+    #find_group_and_check
     @group.destroy
     redirect_to groups_path
   end
 
   private
+
+  def find_group_and_check
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
